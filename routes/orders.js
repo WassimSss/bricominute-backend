987fcb99adcf46d016e18652c3d122d0bb1350ea
Salmi <tojiikan@gmail.com>
2024-03-09 12:42:24 +0100

    order give user idOrder

--- routes/orders.js
+++ routes/orders.js
@@ -1,143 +1,98 @@
 var express = require('express');
 var router = express.Router();
 const uid2 = require('uid2');
-<<<<<<< HEAD
-
-
-const Users = require('../models/user')
+const Users = require('../models/user');
 const orders = require('../models/orders');
-
 
 router.post('/', async (req, res) => {
+	try {
+		const { token, idJob, idJobTask /*status, price, idAddress */ } = req.body;
+		let date = req.body.date;
+		if (!date) {
+			date = new Date();
+		}
 
-    try {
-       
-    const {idUser, idJob, idJobTask,status, price, IdAddress} = req.body
-=======
-const User = require('../models/user')
-const orders = require('../models/orders');
+		if (token && idJob && idJobTask && date /*&& status && price && IdAddress */) {
+			Users.findOne({ token: token }).then(async (findedUser) => {
+				console.log(findedUser._id);
+				if (findedUser) {
+					const newOrders = new orders({
+						idUser: findedUser._id,
+						idJob: idJob,
+						// idPro: null,
+						idJobTask: idJobTask,
+						Date: date,
+						status: false, // Pour l'instant en dur
+						price: 100, // Pour l'instant en dur
+						IdAddress: '65e5ec8fa7d7b53b75681b38' // Pour l'instant en dur
+					});
 
+					// Enregistre la nouvelle commande
+					const data = await newOrders.save();
 
-router.post('/', (req, res) => {
-    const { idUser, idJob, idJobTask, /*status, price, idAddress */ } = req.body
->>>>>>> adress-localisation-pro-consumer
-    let date = req.body.date
-    if (!date) {
-        date = new Date();
-    }
-<<<<<<< HEAD
-    
-    if( idUser && /*idPro && */ idJob && idJobTask && date && status && price && IdAddress) {
-=======
+					const user = await Users.findOne({ token: token });
 
+					// Si l'utilisateur est trouvé, met à jour isOnService à true, sinon à false
+					if (user) {
+						Users.updateOne({ token: token }, { idOrder: data._id }).then((data) => {
+							console.log('data : ', data);
+						});
+						// console.log(user);
+						// await user.save();
+					} else {
+						// Si l'utilisateur n'est pas trouvé, renvoie une réponse avec isOnService à false
+						res.json({ result: false, message: 'Utilisateur non trouvé' });
+						return;
+					}
 
-    if (idUser && idJob && idJobTask && date /*&& status && price && IdAddress */) {
-
->>>>>>> adress-localisation-pro-consumer
-        const newOrders = new orders({
-            idUser: idUser,
-            idJob: idJob,
-            // idPro: null,
-            idJobTask: idJobTask,
-            Date: date,
-<<<<<<< HEAD
-            status: status,
-            price: price, 
-            IdAddress: IdAddress
-        })
-
-            // Enregistre la nouvelle commande
-            const data = await newOrders.save();
-
-            const user = await Users.findById(idUser);
-
-            // Si l'utilisateur est trouvé, met à jour isOnService à true, sinon à false
-            if (user) {
-                Users.updateOne({_id: idUser}, {isOnService: true}).then(
-                    data => {
-                        console.log('data : ', data);
-                    }
-                )
-                // console.log(user);
-                // await user.save();
-            } else {
-                // Si l'utilisateur n'est pas trouvé, renvoie une réponse avec isOnService à false
-                res.json({ result: false, message: 'Utilisateur non trouvé' });
-                return;
-            }
-
-            // Renvoie une réponse avec isOnService à true
-            res.json({ result: true, data: data });
-            
-        
-    } else {
-        // Si des champs requis manquent, renvoie une réponse avec isOnService à false
-        res.json({ result: false, message: 'Champs requis manquants' });
-}
-    } catch (error) {
-        console.error(error);
-        res.json({ result: false, message: 'Erreur' });
-    }
+					// Renvoie une réponse avec isOnService à true
+					res.json({ result: true, data: data });
+				} else {
+					res.json({ result: false, error: 'No user with this token' });
+				}
+			});
+		} else {
+			// Si des champs requis manquent, renvoie une réponse avec isOnService à false
+			res.json({ result: false, message: 'Champs requis manquants' });
+		}
+	} catch (error) {
+		console.error(error);
+		res.json({ result: false, message: 'Erreur' });
+	}
 });
-      module.exports = router;
-=======
-            status: false, // Pour l'instant en dur
-            price: 100, // Pour l'instant en dur
-            IdAddress: '65e5ec8fa7d7b53b75681b38' // Pour l'instant en dur
-        })
-        newOrders.save()
-            .then(data => {
-
-                User.updateOne({ _id: idUser }, { idOrder: data._id })
-                    .then(
-                        res.json({ result: true, data: data })
-                    )
-            })
-    } else {
-        res.json({ result: false })
-
-    }
-
-}
-)
 
 router.get('/getIdAddress/:idOrder', (req, res) => {
-    const idOrder = req.params.idOrder
-    const test = '65eb3e887942b0cc296213b0'
-    console.log(test == idOrder);
-    console.log(test, idOrder);
-    console.log('params : ', typeof req.params.idOrder);
-    orders.findOne({ _id: idOrder })
-        .then(data => {
-            console.log('idOrder : ', data);
-            if (data) {
-                res.json({ result: true, IdAddress: data.IdAddress });
-            }
+	const idOrder = req.params.idOrder;
+	const test = '65eb3e887942b0cc296213b0';
+	console.log(test == idOrder);
+	console.log(test, idOrder);
+	console.log('params : ', typeof req.params.idOrder);
+	orders
+		.findOne({ _id: idOrder })
+		.then((data) => {
+			console.log('idOrder : ', data);
+			if (data) {
+				res.json({ result: true, IdAddress: data.IdAddress });
+			}
 
-            // Utilisez la propriété IdAddress du document retourné
-
-        })
-        .catch(error => {
-            console.error('Error while fetching IdAddress:', error);
-            res.status(500).json({ result: false, error: 'Internal server error' });
-        });
+			// Utilisez la propriété IdAddress du document retourné
+		})
+		.catch((error) => {
+			console.error('Error while fetching IdAddress:', error);
+			res.status(500).json({ result: false, error: 'Internal server error' });
+		});
 });
 
 // isProFinded
 router.get('/isProFinded/:idOrder', (req, res) => {
-    orders.findOne({ _id: req.params.idOrder })
-        .then(data => {
-
-            if (data.idPro) {
-                res.json({ result: true })
-
-            } else {
-                res.json({ result: false })
-
-            }
-            console.log(data);
-            // if(data)
-        })
-})
+	orders.findOne({ _id: req.params.idOrder }).then((data) => {
+		if (data.idPro) {
+			res.json({ result: true });
+		} else {
+			res.json({ result: false });
+		}
+		console.log(data);
+		// if(data)
+	});
+});
 module.exports = router;
->>>>>>> adress-localisation-pro-consumer
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const User = require('../models/user');
-const Orders = require('../models/orders')
+const Orders = require('../models/orders');
 const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
@@ -13,257 +13,217 @@ const geolib = require('geolib');
 
 // const { map } = require('lodash');
 
-
-
 router.post('/upload', async (req, res) => {
-  const pdfPaths = [];
+	const pdfPaths = [];
 
-  try {
-    // Itérer sur tous les fichiers dans la requête
-    for (let i = 1; i <= 3; i++) {
-      const pdfPath = `./tmp/${uniqid()}.pdf`;
-      const resultMove = await req.files[`selectionPDF${i}`].mv(pdfPath);
+	try {
+		// Itérer sur tous les fichiers dans la requête
+		for (let i = 1; i <= 3; i++) {
+			const pdfPath = `./tmp/${uniqid()}.pdf`;
+			const resultMove = await req.files[`selectionPDF${i}`].mv(pdfPath);
 
-      if (!resultMove) {
-        pdfPaths.push(pdfPath);
-      } else {
-        // Si le déplacement du fichier échoue, annulez l'opération
-        throw new Error(resultMove);
-      }
-    }
+			if (!resultMove) {
+				pdfPaths.push(pdfPath);
+			} else {
+				// Si le déplacement du fichier échoue, annulez l'opération
+				throw new Error(resultMove);
+			}
+		}
 
-    // Uploader tous les fichiers sur Cloudinary
-    const uploadPromises = pdfPaths.map(async (path) => {
-      const resultCloudinary = await cloudinary.uploader.upload(path);
-      return resultCloudinary.secure_url;
-    });
+		// Uploader tous les fichiers sur Cloudinary
+		const uploadPromises = pdfPaths.map(async (path) => {
+			const resultCloudinary = await cloudinary.uploader.upload(path);
+			return resultCloudinary.secure_url;
+		});
 
-    // Attendre que toutes les opérations d'upload soient terminées
-    const uploadedUrls = await Promise.all(uploadPromises);
+		// Attendre que toutes les opérations d'upload soient terminées
+		const uploadedUrls = await Promise.all(uploadPromises);
 
-    // Supprimer les fichiers locaux après l'upload
-    pdfPaths.forEach((path) => fs.unlinkSync(path));
+		// Supprimer les fichiers locaux après l'upload
+		pdfPaths.forEach((path) => fs.unlinkSync(path));
 
-    res.json({ result: true, urls: uploadedUrls });
-  } catch (error) {
-    // Gérer les erreurs
-    res.json({ result: false, error: error.message });
-  }
+		res.json({ result: true, urls: uploadedUrls });
+	} catch (error) {
+		// Gérer les erreurs
+		res.json({ result: false, error: error.message });
+	}
 });
-
-
 
 // route qui permet l'inscription d'un particulier
 router.post('/signup', (req, res) => {
-  if (!checkBody(req.body, ['firstName', 'lastName', 'email', 'password',])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
-    return;
-  }
+	if (!checkBody(req.body, [ 'firstName', 'lastName', 'email', 'password' ])) {
+		res.json({ result: false, error: 'Missing or empty fields' });
+		return;
+	}
 
-  // Vérifiez si l'utilisateur n'est pas déjà enregistré
-  User.findOne({ email: req.body.email }).then(data => {
-    if (data === null) {
-      const hash = bcrypt.hashSync(req.body.password, 10);
+	// Vérifiez si l'utilisateur n'est pas déjà enregistré
+	User.findOne({ email: req.body.email }).then((data) => {
+		if (data === null) {
+			const hash = bcrypt.hashSync(req.body.password, 10);
 
-      const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: hash,
-        token: uid2(32),
-        isPro: req.body.isPro,
-      });
+			const newUser = new User({
+				firstName: req.body.firstName,
+				lastName: req.body.lastName,
+				email: req.body.email,
+				password: hash,
+				token: uid2(32),
+				isPro: req.body.isPro,
+			});
 
-      newUser.save().then(newDoc => {
-        res.json({ result: true, token: newDoc.token });
-      });
-    } else {
-      // L'utilisateur existe déjà dans la base de données
-      res.json({ result: false, error: 'User already exists' });
-    }
-  });
+			newUser.save().then((newDoc) => {
+				res.json({ result: true, token: newDoc.token, isPro: newDoc.isPro });
+			});
+		} else {
+			// L'utilisateur existe déjà dans la base de données
+			res.json({ result: false, error: 'User already exists' });
+		}
+	});
 });
 
 router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['email', 'password'])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
-    return;
-  }
+	if (!checkBody(req.body, [ 'email', 'password' ])) {
+		res.json({ result: false, error: 'Missing or empty fields' });
+		return;
+	}
 
-  User.findOne({ email: req.body.email }).then(data => {
-    if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token });
-    } else {
-      res.json({ result: false, error: 'User not found or wrong password' });
-    }
-  });
+	User.findOne({ email: req.body.email }).then((data) => {
+		if (data && bcrypt.compareSync(req.body.password, data.password)) {
+			res.json({ result: true, token: data.token, isPro: data.isPro });
+		} else {
+			res.json({ result: false, error: 'User not found or wrong password' });
+		}
+	});
 });
 
 // Rechercher la position de tout les utilisateurs en ligne
 // Renvoyer l'utilisateur le plus proche de la position
 
 router.get('/findUserNearbyAndGiveOrder/:lat/:long/:idOrder', async (req, res) => {
-  // const locationOrder = JSON.parse(req.params.locationOrder); // Convertir en objet JSON
-  const locationOrder = { latitude: req.params.lat, longitude: req.params.long }
-  const idOrder = req.params.idOrder
+	// const locationOrder = JSON.parse(req.params.locationOrder); // Convertir en objet JSON
+	const locationOrder = { latitude: req.params.lat, longitude: req.params.long };
+	const idOrder = req.params.idOrder;
 
-  try {
-    // Récupérer tous les utilisateurs en ligne depuis la base de données
-    const usersOnline = await User.find({ "professionalInfo.isOnline": true });
-    console.log(usersOnline);
-    let closestUser = null;
-    let minDistance = Infinity;
+	try {
+		// Récupérer tous les utilisateurs en ligne depuis la base de données
+		const usersOnline = await User.find({ 'professionalInfo.isOnline': true });
+		console.log(usersOnline);
+		let closestUser = null;
+		let minDistance = Infinity;
 
-    // Boucler sur chaque utilisateur
-    usersOnline.forEach(user => {
-      console.log(user.professionalInfo.position);
-      console.log(locationOrder);
-      // Calculer la distance entre l'utilisateur et la position de la commande
-      const distance = geolib.getDistance(user.professionalInfo.position, locationOrder);
-      const distanceInKilometers = geolib.convertDistance(distance, 'km');
+		// Boucler sur chaque utilisateur
+		usersOnline.forEach((user) => {
+			console.log(user.professionalInfo.position);
+			console.log(locationOrder);
+			// Calculer la distance entre l'utilisateur et la position de la commande
+			const distance = geolib.getDistance(user.professionalInfo.position, locationOrder);
+			const distanceInKilometers = geolib.convertDistance(distance, 'km');
 
-      // Mettre à jour l'utilisateur le plus proche si nécessaire
-      console.log('distanceInKilometers : ', distanceInKilometers);
-      if (distance < minDistance && distanceInKilometers < 500) { // limite a 10km
-        closestUser = user;
-        minDistance = distanceInKilometers;
-      }
-    });
+			// Mettre à jour l'utilisateur le plus proche si nécessaire
+			console.log('distanceInKilometers : ', distanceInKilometers);
+			if (distance < minDistance && distanceInKilometers < 500) {
+				// limite a 10km
+				closestUser = user;
+				minDistance = distanceInKilometers;
+			}
+		});
 
-    // Renvoyer l'ID de l'utilisateur le plus proche
-    if (closestUser) {
+		// Renvoyer l'ID de l'utilisateur le plus proche
+		if (closestUser) {
+			// Upload l'odrer pour ui attribuer le pro trouvé
+			Orders.updateOne({ _id: idOrder }, { idPro: closestUser._id }).then(
+				User.updateOne({ _id: closestUser._id }, { idOrder: idOrder }).then(
+					// Passer a l'user l'id de la commande en cours
+					User.findOne({ _id: closestUser._id }).then((data) => {
+						res.json({
+							result: true,
+							test: data,
+							user: {
+								firstName: data.firstName,
+								lastName: data.lastName,
+								company_name: data.company_name
+							} /*userId: closestUser._id, distance: distanceInKilometers */
+						});
+					})
+				)
+			);
 
-      // Upload l'odrer pour ui attribuer le pro trouvé
-      Orders.updateOne({ _id: idOrder }, { idPro: closestUser._id })
-        .then(
-          User.updateOne({ _id: closestUser._id }, { idOrder: idOrder })
-            .then(
-              // Passer a l'user l'id de la commande en cours
-              User.findOne({ _id: closestUser._id })
-                .then(data => {
-                  res.json({
-                    result: true, test: data, user: {
-                      firstName: data.firstName,
-                      lastName: data.lastName,
-                      company_name: data.company_name,
-                    }/*userId: closestUser._id, distance: distanceInKilometers */
-                  });
-                })
-            )
-        )
-
-
-      Orders.updateOne({ _id: idOrder }, { idPro: closestUser._id })
-    } else {
-      res.json({ message: "Aucun utilisateur trouvé." });
-    }
-  } catch (error) {
-    console.error('Erreur lors de la recherche de l\'utilisateur le plus proche :', error);
-    res.status(500).json({ error: "Erreur interne du serveur." });
-  }
+			Orders.updateOne({ _id: idOrder }, { idPro: closestUser._id });
+		} else {
+			res.json({ message: 'Aucun utilisateur trouvé.' });
+		}
+	} catch (error) {
+		console.error("Erreur lors de la recherche de l'utilisateur le plus proche :", error);
+		res.status(500).json({ error: 'Erreur interne du serveur.' });
+	}
 });
 
-router.get('/isOnService/:idUser', (req, res) => {
-  const idUser = req.params.idUser
+router.get('/isOnService/:token', (req, res) => {
+	const token = req.params.token;
 
-  User.findOne({ _id: idUser })
-    .then(data => {
-      console.log(data);
+	User.findOne({ token: token }).then((data) => {
+		console.log(data);
 
-      if (data.idOrder) {
-        res.json({ result: true })
-
-      } else {
-        res.json({ result: false })
-
-      }
-    })
-})
-
-
-router.put('/changeIsOnline/:idUser', async (req, res) => {
-  // console.log('coucou');
-  // // Récupère l'ID du professionnel à partir des paramètres de l'URL
-  // const professionalId = req.params.idUser;
-
-
-  // // Recherche et met à jour le professionnel par son ID
-  // User.findOne({_id: professionalId})
-  // .then(data => {
-  //   if(data){
-  //     console.log(data.isOnline, !data.isOnline);
-  //     User.updateOne({email: data.email}, {isOnline: !data.isOnline})
-  //     .then(data => {
-    
-  //       res.json({result: true, user: data})
-  //     })
-  //   } else {
-  //     res.json({result: false, error: 'Utilisateur non trouvé'})
-  //   }
-    
-
-  // })
-
-  try {
-    const professionalId = req.params.idUser;
-    const user = await User.findOne({ _id: professionalId });
-
-    if (!user) {
-      return res.json({ result: false, error: 'Utilisateur non trouvé' });
-    }
-
-    // Inverse la valeur de isOnline
-    user.isOnline = !user.isOnline;
-    await user.save();
-
-    res.json({ result: true, user });
-  } catch (error) {
-    console.error('Erreur lors de la mise à jour :', error);
-    res.status(500).json({ result: false, error: 'Erreur serveur' });
-  }
+		if (data.idOrder) {
+			res.json({ result: true });
+		} else {
+			res.json({ result: false });
+		}
+	});
 });
 
+router.put('/changeIsOnline', async (req, res) => {
+	try {
+		const token = req.body.token;
+		const user = await User.findOne({ token: token });
+
+		if (!user) {
+			return res.json({ result: false, error: 'Utilisateur non trouvé' });
+		}
+
+		console.log(user);
+		// Inverse la valeur de isOnline
+		user.professionalInfo.isOnline = !user.professionalInfo.isOnline;
+		await user.save();
+
+		res.json({ result: true, user });
+	} catch (error) {
+		console.error('Erreur lors de la mise à jour :', error);
+		res.status(500).json({ result: false, error: 'Erreur serveur' });
+	}
+});
 
 // recuperer la propriete isOnline du pro grace a son id
-router.get('/isOnline/:idUser', async (req, res) => {
-  // Chercher le user
-  const professionalId = req.params.idUser;
+router.get('/isOnline/:token', async (req, res) => {
+	// Chercher le user
+	const token = req.params.token;
+	try {
+		const user = await User.findOne({ token: token });
 
-  try {
-    const user = await User.findOne({_id:professionalId })
+		console.log(user);
+		if (!user) {
+			return res.json({ result: false, error: 'Utilisateur non trouvé' });
+		}
 
-    if(!user){
-      return res.json({ result: false, error: 'Utilisateur non trouvé' });
-    }
-
-    console.log(user);
-    res.json({result: true, isOnline: user.isOnline})
-  } catch (error) {
-    console.error('Erreur lors de la mise à jour :', error);
-    res.status(500).json({ result: false, error: 'Erreur serveur' });
-  }
-  // Renvoyer la reponse isOnline
-})
-
-
+		console.log(user);
+		res.json({ result: true, isOnline: user.isOnline });
+	} catch (error) {
+		console.error('Erreur lors de la mise à jour :', error);
+		res.status(500).json({ result: false, error: 'Erreur serveur' });
+	}
+	// Renvoyer la reponse isOnline
+});
 
 // Recuperer le isOnService du user grace a un id
 
 router.get('/getIsOnService/:idUser', async (req, res) => {
-  try {
-    const user = await User.findOne({_id : req.params.idUser})
-    
-    if(!user){
-      return res.json({result : false, error: 'User non trouvé'})
-    }
-    
-    res.json({result: true, isOnService: user.isOnService})
+	try {
+		const user = await User.findOne({ _id: req.params.idUser });
 
-  } catch (error) {
-    
-  }
+		if (!user) {
+			return res.json({ result: false, error: 'User non trouvé' });
+		}
 
-})
+		res.json({ result: true, isOnService: user.isOnService });
+	} catch (error) {}
+});
 module.exports = router;
-
-
