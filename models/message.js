c287387b7d674a64fae19fddf41790a6d6d5aa13
isma <mae_baby@hotmail.fr>
2024-03-14 11:45:18 +0100

    messagerie

---
+++ models/message.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = mongoose.Schema({
+    idUser: { type: mongoose.Schema.Types.ObjectId, ref: 'users'},
+    idUserReceiver: { type: mongoose.Schema.Types.ObjectId, ref: 'users'},
+    message:String,
+    date:Date,
+    
+});
+const Message = mongoose.model('Message', MessageSchema);
+module.exports = Message;
